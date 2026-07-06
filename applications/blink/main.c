028af12f50985e4dcc561c940b07368cf2850b67
MOHAN SUDHANDHIAM M <mohansudhandhiram.m2025ece@sece.ac.in>
2026-07-06 09:57:51 +0000

    feat(gpio): implement bare-metal LED blink driver for STM32F1

--- applications/blink/main.c
+++ applications/blink/main.c
@@ -1,8 +1,23 @@
-int main(void)
-{
-    while(1)
-    {
-        // Test main
+#include <stdint.h>
+
+// Base Addresses (STM32F1)
+#define RCC_BASE      0x40021000   
+#define GPIOA_BASE    0x40010800   
+
+#define RCC_APB2ENR   (*((volatile uint32_t *)(RCC_BASE + 0x18)))
+#define GPIOA_CRL     (*((volatile uint32_t *)(GPIOA_BASE + 0x00)))
+#define GPIOA_ODR     (*((volatile uint32_t *)(GPIOA_BASE + 0x0C)))
+
+int main(void) {
+
+    RCC_APB2ENR |= (1 << 2);
+    GPIOA_CRL &= ~(0xF << 20); 
+    GPIOA_CRL |= (1 << 21);
+    
+    while(1) { 
+        GPIOA_ODR ^= (1 << 5);
+        for(volatile int i = 1000000; i > 0;i--);
     }
+
     return 0;
 }
